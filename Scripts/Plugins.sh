943a7da3134c489d054f5e58059a00736ccd871e
herofence <113574064+herofence@users.noreply.github.com>
2023-05-14 11:08:09 +0800

    Create Plugins.sh

---
+++ Scripts/Plugins.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Argon Theme
+git clone --depth=1 --single-branch --branch "18.06" https://github.com/jerrykuku/luci-theme-argon.git
+#Argon Config
+git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-app-argon-config.git
+#Design Theme
+git clone --depth=1 --single-branch https://github.com/gngpp/luci-theme-design.git
+#Design Config
+git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
+#第三方源
+git clone --depth=1 --single-branch  https://github.com/kenzok8/small-package
+#istore
+git clone --depth=1 --single-branch https://github.com/linkease/istore
+git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci
+git clone --depth=1 --single-branch https://github.com/linkease/nas-packages
+#8812cu
+#git clone --depth=1 --single-branch https://github.com/srdgame/rtl8812cu-20200609
