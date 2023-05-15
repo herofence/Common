019ad3ac853cd8e43dd1077e21fdb5114035289d
herofence <113574064+herofence@users.noreply.github.com>
2023-05-15 13:07:34 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -11,4 +11,4 @@ git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-app-argon-
 git clone --depth=1 --single-branch https://github.com/gngpp/luci-theme-design.git
 git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
 #Passwall
-git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwall.git
+git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
