89dad742796813c3d9b6d12b2751dbef2dc9e497
herofence <113574064+herofence@users.noreply.github.com>
2023-05-18 21:08:51 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -5,8 +5,8 @@
 #git clone --depth=1 --single-branch https://github.com/linkease/nas-packages.git
 #git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci.git
 #Argon Theme
-git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-theme-argon.git
-git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-app-argon-config.git
+#git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-theme-argon.git
+#git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-app-argon-config.git
 #Design Theme
 git clone --depth=1 --single-branch https://github.com/gngpp/luci-theme-design.git
 git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
