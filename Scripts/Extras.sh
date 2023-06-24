3b285c794a348f4bbaf0322386c77f0266205f3a
herofence <113574064+herofence@users.noreply.github.com>
2023-06-24 13:04:10 +0800

    Update Extras.sh

--- Scripts/Extras.sh
+++ Scripts/Extras.sh
@@ -2,7 +2,7 @@
 
 #增加主题
 echo "CONFIG_PACKAGE_luci-theme-$OWRT_THEME=y" >> .config
-echo "CONFIG_PACKAGE_luci-app-$OWRT_THEME-config=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-argon-config=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-design-config=y" >> .config
 #根据源码来修改
 if [[ $OWRT_URL != *"lede"* ]] ; then
