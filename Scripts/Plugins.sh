1ed9070c7fe8b0591436391da654df016bbc2bb2
herofence <113574064+herofence@users.noreply.github.com>
2024-01-20 16:04:39 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -21,3 +21,12 @@ git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwal
 #git clone --depth=1 --single-branch https://github.com/fw876/helloworld.git
 #应用过滤器
 #git clone --depth=1 --single-branch https://github.com/destan19/OpenAppFilter.git
+#openclass
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull origin master
+git branch --set-upstream-to=origin/master master
