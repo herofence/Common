869c231e1a0f53161a02e3e693ba1acc9d88d2a7
herofence <113574064+herofence@users.noreply.github.com>
2024-01-21 13:15:05 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -11,8 +11,8 @@ git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-con
 git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-app-argon-config.git
 #Pass Wall
-git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwall.git
-git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwall-packages.git
+#git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwall.git
+#git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwall-packages.git
 #Hello World
 #git clone --depth=1 --single-branch https://github.com/fw876/helloworld.git
 #应用过滤器
