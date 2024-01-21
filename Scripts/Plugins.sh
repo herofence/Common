b339c6f850502451c197f2cd458102777f6bb579
herofence <113574064+herofence@users.noreply.github.com>
2024-01-21 16:10:20 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -11,49 +11,9 @@ git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-con
 git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-app-argon-config.git
 #Pass Wall
-#git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwall.git
-#git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwall-packages.git
+git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwall.git
+git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwall-packages.git
 #Hello World
 #git clone --depth=1 --single-branch https://github.com/fw876/helloworld.git
 #应用过滤器
 #git clone --depth=1 --single-branch https://github.com/destan19/OpenAppFilter.git
-#预置OpenClash内核和数据
-if [ -d *"OpenClash"* ]; then
-	CORE_VER="https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version"
-	CORE_TYPE=$(echo $WRT_TARGET | egrep -iq "64|86" && echo "amd64" || echo "arm64")
-	CORE_TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
-	CORE_DEV="https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux-$CORE_TYPE.tar.gz"
-	CORE_MATE="https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux-$CORE_TYPE.tar.gz"
-	CORE_TUN="https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux-$CORE_TYPE-$CORE_TUN_VER.gz"
-	GEO_MMDB="https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb"
-	GEO_SITE="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat"
-	GEO_IP="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat"
-	GEO_META="https://github.com/MetaCubeX/meta-rules-dat/raw/release/geoip.metadb"
-	cd ./OpenClash/luci-app-openclash/root/etc/openclash/
-	curl -sfL -o Country.mmdb $GEO_MMDB
-	curl -sfL -o GeoSite.dat $GEO_SITE
-	curl -sfL -o GeoIP.dat $GEO_IP
-	curl -sfL -o GeoIP.metadb $GEO_META
-	mkdir ./core/ && cd ./core/
-	curl -sfL -o meta.tar.gz $CORE_MATE && tar -zxf meta.tar.gz && mv -f clash clash_meta
-	curl -sfL -o tun.gz $CORE_TUN && gzip -d tun.gz && mv -f tun clash_tun
-	curl -sfL -o dev.tar.gz $CORE_DEV && tar -zxf dev.tar.gz
-	chmod +x ./clash* && rm -rf ./*.gz
-	echo "openclash date has been updated!"
-fi
-UPDATE_PACKAGE() {
-	local PKG_NAME=$1
-	local PKG_REPO=$2
-	local PKG_BRANCH=$3
-	local PKG_SPECIAL=$4
-	local REPO_NAME=$(echo $PKG_REPO | cut -d '/' -f 2)
-	rm -rf $(find ../feeds/luci/ -type d -iname "*$PKG_NAME*" -prune)
-	git clone --depth=1 --single-branch --branch $PKG_BRANCH "https://github.com/$PKG_REPO.git"
-	if [[ $PKG_SPECIAL == "pkg" ]]; then
-		cp -rf $(find ./$REPO_NAME/ -type d -iname "*$PKG_NAME*" -prune) ./
-		rm -rf ./$REPO_NAME
-	elif [[ $PKG_SPECIAL == "name" ]]; then
-		mv -f $REPO_NAME $PKG_NAME
-	fi
-}
-UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev"
