d73923c363f4f1bfcb55555795b93fba45477a79
herofence <113574064+herofence@users.noreply.github.com>
2024-10-24 12:39:54 +0800

    Update Plugins.sh

--- scripts/Plugins.sh
+++ scripts/Plugins.sh
@@ -52,7 +52,7 @@ function git_sparse_clone() {
 git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
 # 主题
 git clone --depth=1 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
-git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone https://github.com/y9858/luci-theme-opentomcat package/luci-theme-opentomcat
 # smartdns
