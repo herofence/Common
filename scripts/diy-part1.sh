470dc0ec3ab459e3d9d6b0ba465a1bd568b77b4a
herofence <113574064+herofence@users.noreply.github.com>
2026-08-12 19:08:06 +0800

    Create diy-part1.sh for package management
    This script merges packages, clones community packages, and adds luci-app-mosdns.

---
+++ scripts/diy-part1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Merge_package
+function merge_package(){
+    repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
+    pkg=`echo $2 | rev | cut -d'/' -f 1 | rev`
+    # find package/ -follow -name $pkg -not -path "package/openwrt-packages/*" | xargs -rt rm -rf
+    git clone --depth=1 --single-branch $1
+    [ -d package/openwrt-packages ] || mkdir -p package/openwrt-packages
+    mv $2 package/openwrt-packages/
+    rm -rf $repo
+}
+
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config
+
+# Clone community packages to package/community
+mkdir package/community
+pushd package/community
+git clone --depth=1 https://github.com/fw876/helloworld
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2
+git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki
+git clone --depth=1 https://github.com/DHDAXCW/dhdaxcw-app
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
+git clone --depth=1 https://github.com/DHDAXCW/istore
+git clone --depth=1 https://github.com/Siriling/5G-Modem-Support && rm -rf 5G-Modem-Support/rooter
+git clone --depth=1 https://github.com/gdy666/luci-app-lucky
+popd
+
+# add luci-app-mosdns
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
