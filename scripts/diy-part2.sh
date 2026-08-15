980a5892407d1c37a238b7392a6cceee49c20b99
herofence <113574064+herofence@users.noreply.github.com>
2026-08-15 22:07:49 +0800

    Update dockerd package to stable version
    Removed existing dockerd package and replaced it with a stable version from the OpenWrt repository.

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -3,3 +3,8 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i "s/ImmortalWrt/OpenWrt/g" package/base-files/files/bin/config_generate
+# 修复或重置 dockerd 包，使用稳定来源
+rm -rf feeds/packages/utils/dockerd
+git clone https://github.com/openwrt/packages.git --depth=1 /tmp/openwrt-packages
+cp -r /tmp/openwrt-packages/utils/dockerd feeds/packages/utils/dockerd
+rm -rf /tmp/openwrt-packages
