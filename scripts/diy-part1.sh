90b6fa80948b4060b8515581d8a9ceeb1d718152
herofence <113574064+herofence@users.noreply.github.com>
2026-08-15 11:52:35 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -30,7 +30,6 @@ git clone --depth=1 https://github.com/linkease/nas-packages package/nas-package
 git clone --depth=1 https://github.com/linkease/nas-packages-luci package/nas-packages-luci
 git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
 git clone https://github.com/lmq8267/luci-app-vnt.git package/vnt
-git clone https://github.com/cg8-5712/vnt.git package/vnt
 git clone --depth=1 https://github.com/EasyTier/luci-app-easytier package/luci-app-easytier
 git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
 git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
