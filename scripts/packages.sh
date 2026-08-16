94c8f48c9e9560e1186046374ac3922ac259ef63
herofence <113574064+herofence@users.noreply.github.com>
2026-08-16 19:46:46 +0800

    Add script to manage package dependencies
    This script removes specific plugins from feeds, clones necessary dependencies, and organizes them into a 'small' folder.

---
+++ scripts/packages.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+#删除feeds中的插件
+rm -rf feeds/packages/lang/golang
+rm -rf feeds/packages/net/{geoview,chinadns-ng,hysteria,mosdns,v2ray-geodata}
+rm -rf feeds/packages/net/{shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev}
+rm -rf feeds/packages/net/{sing-box,v2ray-geodata,v2ray-plugin,xray-core}
+rm -rf feeds/luci/applications/{luci-app-mosdns,luci-app-passwall}
+
+#克隆依赖插件
+git clone --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git package/pwpage
+git clone --depth 1 https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
+
+
+#克隆的源码放在small文件夹
+mkdir package/small
+pushd package/small
+
+# luci-theme-aurora
+git clone -b master --depth 1 https://github.com/eamonxg/luci-theme-aurora.git
+
+# luci-app-nft-timecontrol
+git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-timecontrol.git
+
+# adguardhome
+# git clone -b 2024.09.05 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
+
+# lucky
+git clone -b main --depth 1 https://github.com/gdy666/luci-app-lucky.git
+
+# smartdns
+# git clone -b master --depth 1 https://github.com/pymumu/luci-app-smartdns.git
+# git clone -b master --depth 1 https://github.com/pymumu/smartdns.git
+
+# ssrp
+# git clone -b master --depth 1 https://github.com/fw876/helloworld.git
+
+# VIKINGYFY/packages
+git clone -b main --depth 1 https://github.com/VIKINGYFY/packages.git
+
+# passwall
+git clone -b main --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall.git
+
+# passwall2
+git clone -b main --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall2.git
+
+# mosdns
+git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
+
+# luci-app-netspeedtest
+git clone -b master --depth 1 https://github.com/muink/luci-app-netspeedtest.git
+
+# openclash
+# git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
+
+# OpenWrt-nikki
+git clone -b main --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki.git
+
+# OpenWrt-momo
+git clone -b main --depth 1 https://github.com/nikkinikki-org/OpenWrt-momo.git
+
+# daed
+git clone -b master --depth 1 https://github.com/QiuSimons/luci-app-daed.git
+
+popd
+
+echo "packages executed successfully!"
