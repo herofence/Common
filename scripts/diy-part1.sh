8926d26b06189bdb23f4a606243c49b13481d58b
herofence <113574064+herofence@users.noreply.github.com>
2026-08-21 07:18:17 +0800

    Refactor script for IP and Git token configuration

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
-# 修改默认IP
+
+# 1. 修改默认IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-# 设置 Git 使用 token 认证（如果存在）
+
+# 2. 设置 Git 使用 token 认证（如果存在）
 if [ -n "${GITHUB_TOKEN}" ]; then
     git config --global url."https://oauth2:${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
 fi
 
-# Merge_package
-function merge_package(){
-    repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
-    pkg=`echo $2 | rev | cut -d'/' -f 1 | rev`
-    git clone --depth=1 --single-branch $1
-    [ -d package/openwrt-packages ] || mkdir -p package/openwrt-packages
-    mv $2 package/openwrt-packages/
-    rm -rf $repo
-}
-
-# 清理冲突的主题和应用
+# 3. 清理冲突的主题和应用
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
 rm -rf feeds/packages/net/v2ray-geodata
 
-# 克隆插件包到 package目录
+# 4. 克隆第三方插件包到 package/community 目录
 mkdir -p package/community
 pushd package/community
+
+# 主题与常用应用
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
 git clone --depth=1 https://github.com/y9858/luci-theme-opentomcat
-git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns
 git clone --depth=1 https://github.com/lisaac/luci-app-diskman
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky
 git clone --depth=1 https://github.com/sirpdboy/netspeedtest
@@ -37,34 +30,36 @@ git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpu
 git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff
 git clone --depth=1 https://github.com/Jason6111/luci-app-netdata
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages
+
+# SmartDNS (保留单个官方推荐源)
+git clone --depth=1 https://github.com/pymumu/luci-app-smartdns
+
+# OpenClash
+git clone --depth=1 https://github.com/vernesong/OpenClash.git
+
 popd
 
-# 克隆其它仓库
-git clone --depth=1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
-
-# 更改默认主题
+# 5. 更改默认主题为 Argon
 sed -i 's/luci-theme-design/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# x86 型号只显示 CPU 型号
+# 6. x86 型号只显示 CPU 型号
 if [ -f "package/immortalwrt/autocore/files/x86/autocore" ]; then
     sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/immortalwrt/autocore/files/x86/autocore
 elif [ -f "package/lean/autocore/files/x86/autocore" ]; then
     sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/files/x86/autocore
 fi
 
-# 修改本地时间格式
+# 7. 修改本地时间格式
 find package/ -path "*/autocore/files/*/index.htm" -exec sed -i 's#os.date()#os.date("%Y-%m-%d %H:%M:%S") .. " " .. translate(os.date("%A"))#g' {} \; 2>/dev/null
 find feeds/luci/ -path "*/system.lua" -exec sed -i 's/os.date("%c")/os.date("%Y-%m-%d %H:%M:%S")/g' {} \; 2>/dev/null
 
-# 最大连接数修改为65535
+# 8. 最大连接数修改为 65535
 if [ -f "package/base-files/files/etc/sysctl.conf" ]; then
     sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
 fi
 
-# 修改版本为编译日期
+# 9. 修改版本号展示
 date_version=$(date +"%y.%m.%d")
 if [ -f "package/immortalwrt/default-settings/files/zzz-default-settings" ]; then
     orig_version=$(cat "package/immortalwrt/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
@@ -74,7 +69,7 @@ elif [ -f "package/lean/default-settings/files/zzz-default-settings" ]; then
     sed -i "s/${orig_version}/R${date_version} by herofence/g" package/lean/default-settings/files/zzz-default-settings
 fi
 
-# 取消主题默认设置
+# 10. 取消主题默认配置覆盖
 find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \; 2>/dev/null
 
 echo "脚本执行完成"
