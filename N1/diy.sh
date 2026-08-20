4c1cb4d82ffe8a6d68c4be181fdb83b93c9dfbd7
herofence <113574064+herofence@users.noreply.github.com>
2026-08-20 20:08:41 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -2,19 +2,23 @@
 
 # Remove packages
 #rm -rf feeds/packages/net/v2ray-geodata
-rm -rf feeds/luci/applications/luci-app-daed
+#rm -rf feeds/luci/applications/luci-app-daed
 
 #去除自动升级
 sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
 
+# 添加源
+#echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
+
 # Add packages
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
+git clone https://github.com/kenzok8/openwrt-clashoo.git package/openwrt-clashoo
 #git clone  https://github.com/linkease/luci-app-linkease package/linkease
 #git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
 #git_sparse_clone main https://github.com/kenzok8/small-package luci-app-floatip floatip
 #git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-onliner
-git_sparse_clone https://github.com/VIKINGYFY/packages luci-app-homeproxy
-git clone https://github.com/QiuSimons/luci-app-daed package/daed
+#git_sparse_clone https://github.com/VIKINGYFY/packages luci-app-homeproxy
+#git clone https://github.com/QiuSimons/luci-app-daed package/daed
 
 # 加入OpenClash核心
 #chmod -R a+x $GITHUB_WORKSPACE/preset-clash-core.sh
@@ -23,16 +27,9 @@ git clone https://github.com/QiuSimons/luci-app-daed package/daed
 echo "
 # 插件
 CONFIG_PACKAGE_luci-app-amlogic=y
-CONFIG_PACKAGE_luci-app-homeproxy=y
 CONFIG_PACKAGE_luci-app-openclash=y
-# daed
-CONFIG_BPF_TOOLCHAIN_HOST=y
-CONFIG_DEVEL=y
-CONFIG_KERNEL_BPF_EVENTS=y
-CONFIG_KERNEL_DEBUG_INFO_BTF=y
-CONFIG_KERNEL_DEBUG_INFO_REDUCED=n
-CONFIG_KERNEL_XDP_SOCKETS=y
-CONFIG_PACKAGE_luci-app-daed=y
+CONFIG_PACKAGE_luci-app-clashoo=y
+#CONFIG_PACKAGE_luci-app-nikki=y
 " >> .config
 
 # 修改默认IP
